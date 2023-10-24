d7c1703d3be83b47e686c08b6fc3b8f43a5c7b78
GitMarkPaul <markpaulcaneda1997@gmail.com>
2023-10-24 11:46:30 +0800

    ui updates

--- pages/admission/applicant-type.vue
+++ pages/admission/applicant-type.vue
@@ -14,7 +14,9 @@
 		<div class="card">
 			<div class="card-body">
 				<div class="text-start">
-					<img class="w-auto h-8 sm:h-12" src="/logo.png" alt="">
+					<NuxtLink to="/" class="bg-gray-200 text-gray-600 rounded-full flex items-center justify-center p-1 w-10 h-10">
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M10.733 19.79a.75.75 0 0 0 1.034-1.086L5.516 12.75H20.25a.75.75 0 0 0 0-1.5H5.516l6.251-5.955a.75.75 0 0 0-1.034-1.086l-7.42 7.067a.995.995 0 0 0-.3.58a.754.754 0 0 0 .001.289a.995.995 0 0 0 .3.579l7.419 7.067Z"/></svg>
+					</NuxtLink>
 					<h2 class="mt-3 text-2xl font-semibold text-gray-800 capitalize sm:text-3xl dark:text-white">Applicant Type</h2>
 					<p class="mt-3 text-gray-500 dark:text-gray-300">Tell us what type of account you'd be opening.</p>
 				</div>
