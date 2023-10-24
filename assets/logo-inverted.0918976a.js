b772f0cf3db37387d995628b36fcbf40237435c5
GitMarkPaul <markpaulcaneda1997@gmail.com>
2023-10-24 13:26:16 +0800

    Updates

--- assets/logo-inverted.0918976a.js
+++
@@ -1,1 +0,0 @@
-import"./entry.3978a5fb.js";const s=""+globalThis.__publicAssetsURL("logo-inverted.png");export{s as _};
---
+++ assets/logo.0d8313f5.js
@@ -0,0 +1,1 @@
+import"./entry.5e68a7c9.js";const s=""+globalThis.__publicAssetsURL("logo.png");export{s as _};
